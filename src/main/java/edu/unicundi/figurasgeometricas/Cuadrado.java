e637bbe59597ce7f5224e4ebad80a7aac2129d49
David Márquez <davidmarquezing@gmail.com>
2020-02-27 21:56:10 -0500

    Documentar clases Cuadrado, Triangulo, Rectangulo

--- src/main/java/edu/unicundi/figurasgeometricas/Cuadrado.java
+++ src/main/java/edu/unicundi/figurasgeometricas/Cuadrado.java
@@ -7,21 +7,33 @@ package edu.unicundi.figurasgeometricas;
 
 /**
  *
- * @author PROFESIONAL
+ * @author David Márquez Clase para manejar los metodos del cuadrado y extiende
+ * de la clase Cuadrilateros
  */
 public class Cuadrado extends Cuadrilateros {
 
+    /**
+     * Constructor principal de la clase
+     *
+     * @param coorde1 Almacena la coordenada 1
+     * @param coorde2 Almacena la coordenada 2
+     * @param coorde3 Almacena la coordenada 3
+     * @param coorde4 Almacena la coordenada 4
+     */
     public Cuadrado(String coorde1, String coorde2, String coorde3, String coorde4) {
         super(coorde1, coorde2, coorde3, coorde4);
 
     }
 
+    /**
+     * Metodo para imprimir la informacion de area y perimetro del cuadrado
+     */
     @Override
     public void imprimirInfo() {
         if (separarCoordenadas() == true && getLado1() == getLado2()) {
             System.out.println("Perimetro " + getPerimetro());
             System.out.println("Area " + getArea());
-        }else{
+        } else {
             System.out.println("No es un cuadrado");
         }
 
--- src/main/java/edu/unicundi/figurasgeometricas/Rectangulo.java
+++ src/main/java/edu/unicundi/figurasgeometricas/Rectangulo.java
@@ -7,14 +7,26 @@ package edu.unicundi.figurasgeometricas;
 
 /**
  *
- * @author user
+ * @author David Márquez Clase para manejar los metodos del rectangulo y
+ * extiende de la clase padre Cuadrilateros
  */
 public class Rectangulo extends Cuadrilateros {
 
+    /**
+     * Constructor principal de la clase
+     *
+     * @param coorde1 almacena la coordenada 1
+     * @param coorde2 almacena la coordenada 2
+     * @param coorde3 almacena la coordenada 3
+     * @param coorde4 almacena la coordenada 4
+     */
     public Rectangulo(String coorde1, String coorde2, String coorde3, String coorde4) {
         super(coorde1, coorde2, coorde3, coorde4);
     }
 
+    /**
+     * Metodo para imprimir la informacion de area y el perimetro del rectangulo
+     */
     @Override
     public void imprimirInfo() {
         if (separarCoordenadas() == true && getLado1() != getLado2()) {
@@ -25,6 +37,9 @@ public class Rectangulo extends Cuadrilateros {
         }
     }
 
+    /**
+     * Metodo para hallar el perimetro del rectangulo
+     */
     @Override
     public void hallarPerimetro() {
         setPerimetro((getLado2() * 2) + (getLado1() * 2));
